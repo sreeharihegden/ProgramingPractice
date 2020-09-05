0422388ff300d287a958823ca6eea522778deab9
sreeharihegden <sreeharihegden@users.noreply.github.com>
2020-09-05 12:27:44 -0400

    Updated Palindrome result printing.

--- palindromeNumber.c
+++ palindromeNumber.c
@@ -11,7 +11,7 @@ int main(){
 		reversedNum = (reversedNum * 10) + currentDigit;
 		copyn1 = copyn1/10;
 	}
-	printf("Reversed number is %d.", reversedNum);
+	printf("Reversed number is %d.\n", reversedNum);
 	if(n1 == reversedNum){
 		printf("%d is palindrome.", n1);
 	}
